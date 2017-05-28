968ff09cb1953042950a94f91d8abd6a2659d52e
Joao <joao4018@gmail.com>
2017-05-27 23:40:38 -0300

    Att

--- src/ep2/Aliens.java
+++ src/ep2/Aliens.java
@@ -6,7 +6,9 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Aliens extends Sprite {
     
     private static final int LARGURA_TELA = 500;
-    private static final int VELOCIDADE = 2;
+    private int VELOCIDADE = 2;
+    private int dano;
+    private int score;
     
     public Aliens(int x, int y) {
         super(x, y);
@@ -18,8 +20,47 @@ public class Aliens extends Sprite {
     }
     
     private void Alen(){
-        loadImage("images/alien_EASY.png"); 
         
+         
+          setImagem(0);
+         
+    }
+    
+    public void setImagem(int qtd){
+        if(qtd <= 20){
+        loadImage("images/alien_EASY.png");
+        setDanos(1); 
+        setScore(1);
+        }
+        else if(qtd > 20 && qtd <= 40){
+        loadImage("images/alien_MEDIUM.png");
+        setVelocidade(4);
+        setDanos(2); 
+        setScore(2);
+        }
+        else {
+        loadImage("images/alien_HARD.png");
+        setVelocidade(8);
+        setDanos(4); 
+        setScore(5);
+        }
+    }
+    
+    
+    
+    public void setDanos(int i){
+        this.dano = i;
+    }
+    
+    public int getDanos(){
+        return dano;
+    }
+     public void setScore(int i){
+        this.score = i;
+    }
+    
+    public int getScore(){
+        return score;
     }
     
     public void explode(){
@@ -28,14 +69,15 @@ public class Aliens extends Sprite {
     
     public void move() {
         // Moves the spaceship on the verical axis
-        if(this.y > LARGURA_TELA){
-            this.y = 0;
-          
-            
-        }
-        y += VELOCIDADE;
+        
+        y += getVelocidade();
         
     }
-    
+    public void setVelocidade(int qtd){
+        VELOCIDADE = qtd;
+    } 
+    public int getVelocidade(){
+        return VELOCIDADE;
+    }
 }
 
